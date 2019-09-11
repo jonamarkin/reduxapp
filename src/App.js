f5a1a7d32f827847c07184d534539a4a6df8a75d
unknown <jonamarkin>
2019-09-10 17:01:30 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import {increment, decrement, login} from './Actions'
+
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Counter {counter}</p>
+      <button onClick = {() => dispatch(increment(5))}>+</button>
+      <button onClick = {() => dispatch(decrement())}>-</button>
+      <button onClick = {() => dispatch(login())}>See Something</button>
+
+      {isLogged ? <h3>Valuable information I couldn't see</h3> : ''}
+      {/* <h3>Valuable information I couldn't see</h3> */}
     </div>
   );
 }
